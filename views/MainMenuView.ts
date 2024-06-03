6280f0be0644e9e6a7fa7088f19adf59ba1a8bd6
March222 <marcello.belli.stud@ispascalcomandini.it>
2024-06-03 12:50:37 +0200

    Aggiunta della libreria per le domande e fine dei primi due punti del View

--- views/MainMenuView.ts
+++ views/MainMenuView.ts
@@ -1,11 +1,34 @@
+import * as rl from 'readline-sync'
+import { ParcheggiController } from '../controllers/ParcheggiController';
 export class MainMenuView
 {
     static show()
     {
+        let controller=new ParcheggiController();
+        let scelta:number=0;
+        do{
         console.log('GESTIONE PARCHEGGIO'); 
         console.log('---------------------\n');
         console.log('1 - Elenco Veicoli');
         console.log('2 - Numero veicoli presenti\n');
-        console.log('9 - Esci')
+        console.log('9 - Esci\n')
+
+        scelta=rl.questionInt('Inserire la scelta:');
+
+        switch(scelta)
+        {
+            case 1:
+                controller.getAll().forEach(v=>console.log(`${v.targa} -Entrato ${v.ingresso}`));
+                break;
+            case 2:
+                console.log(controller.getNumber());
+                break;
+            case 9:
+                break;
+            default:
+                console.log('Scelta non valida');
+                break;
+        }
+    }while(scelta!=9);
     }
 }
